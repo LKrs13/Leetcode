908099eab262262b94111291fd94be90a7f7226a
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-08-24 18:53:14 -0400

    Time: 156 ms (47.21%), Space: 14 MB (35.29%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,30 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows, cols = len(board), len(board[0])
         
-        for r in range(rows):
-            row = [x for x in board[r] if x.isdigit()]
-            if len(set(row)) != len(row):
+        def repetitionChecker(row):
+            ct = Counter(row)
+            for i, j in ct.items():
+                if i != "." and j > 1:
+                    return False
+            return True
+        
+        for row in board:
+            if not repetitionChecker(row):
                 return False
         
-        transposed = []
-        for r in range(rows):
-            row = []
-            for c in range(cols):
-                row.append(board[c][r])
-            transposed.append(row)
-        
-        for r in range(rows):
-            row = [x for x in transposed[r] if x.isdigit()]
-            if len(set(row)) != len(row):
+        t = list(zip(*board))
+        for col in t:
+            if not repetitionChecker(col):
                 return False
         
-        for r in range(0, rows, 3):
-            for c in range(0, cols, 3):
-                box = board[r][c:c+3] + board[r+1][c:c+3] + board[r+2][c:c+3]
-                box = [x for x in box if x.isdigit()]
-                if len(set(box)) != len(box):
+        for r in range(0, 9, 3):
+            for i in range(0, 9, 3):
+                x = board[r][i:i+3] + board[r+1][i:i+3] + board[r+2][i:i+3]
+                if not repetitionChecker(x):
                     return False
         
         return True 
-                
