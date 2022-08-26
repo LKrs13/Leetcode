d6ca532bd799eb8b7aaa5bf7c0206eee94dffa0e
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-08-26 00:03:41 -0400

    Time: 716 ms (43.14%), Space: 32.4 MB (13.57%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,40 +1,29 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.parent = self
-        self.size = 1
-    
-class UnionFind:
-        
-    def find(self, node):
-        if node.parent != node:
-            node.parent = self.find(node.parent)
-        return node.parent
-    
-    def union(self, node1, node2):
-        parent_1 = self.find(node1)
-        parent_2 = self.find(node2)
-        if parent_1 != parent_2:
-            parent_2.parent = parent_1
-            parent_1.size += parent_2.size
-        return parent_1.size
-                
-        
-        
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        uf = UnionFind()
-        nodes = {}
-        max_size = 0
-        for num in nums:
-            if num not in nodes:
-                node = Node(num)
-                nodes[num] = node
-                size = 1
-                if num + 1 in nodes:
-                    size = uf.union(node, nodes[num+1])
-                if num - 1 in nodes:
-                    size = uf.union(node, nodes[num-1])
-                max_size = max(max_size, size)
+        res = 0
+        dc = {}
+        
+        for n in nums:
+            dc[n] = float(inf)
+            
+            if n+1 in dc:
+                dc[n] = (n+1)
+            
+            if n-1 in dc:
+                dc[n-1] = (n)
+        
+        for i, j in dc.items():
+            
+            if i-1 in dc:
+                continue
                 
-        return max_size
+            tmp = j
+            ct = 1
+            
+            while tmp in dc and tmp != float('inf'):
+                ct += 1
+                tmp = dc[tmp]
+                
+            res = max(res, ct)
+        
+        return res
