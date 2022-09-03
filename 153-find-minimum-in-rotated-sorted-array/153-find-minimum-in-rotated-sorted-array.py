42eccd8cea5a3e55f698fbdde245aaaa2aadb6ca
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-09-02 21:18:20 -0400

    Time: 40 ms (96.48%), Space: 14.2 MB (63.52%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,10 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        
+        # [1,2,3,4,5] 1
+        # [2,3,4,5,1] 2
+        # [3,4,5,1,2] 2
+        # [4,5,1,2,3] 1
+        # [5,1,2,3,1] 2
+        # want to look at side that is not sorted
+        # since we are guaranteed to find a bigger number 
+        # in the sorted side
+        
         l, r = 0, len(nums)-1
         
         while l < r:
             m = (l + r) // 2
             
+            # right side is sorted (increasing) => want to look left to find smaller
             if nums[m] < nums[r]:
                 r = m
             
@@ -12,3 +23,4 @@ class Solution:
                 l = m + 1
             
         return nums[l]
+    
