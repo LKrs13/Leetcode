667d60694b370a6ba9892785a83e04d13fcd21f4
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-09-05 21:50:37 -0400

    Time: 234 ms (32.57%), Space: 14 MB (31.39%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,13 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        "aacd", "erdsgacadbsr"
-        
-        ct = Counter(s1)
-        
-        for i in range(len(s2)):
-            if s2[i] in ct:
-                dc = Counter(s2[i:i+len(s1)])
-                if dc == ct:
-                    return True
-        
+        d1 = Counter(s1)
+        k = len(s1)
+
+        window = s2[:k]
+        d2 = Counter(window)
+
+        if d1 == d2:
+            return True
+
+        for i in range(len(s2)-k):
+
+            d2[s2[i]] -= 1
+            d2[s2[i+k]] += 1   
+
+            if d1 == d2:
+                return True
+            
         return False
