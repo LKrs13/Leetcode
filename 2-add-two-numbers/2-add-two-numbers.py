ab6ee42bb0a8b078f7066fa136ab323290ef4f75
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-09-29 23:15:53 -0400

    Time: 150 ms (21.15%), Space: 14.1 MB (9.83%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -1,23 +1,22 @@
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-    Let l1 be the list we will return, and keep replacing values on l1. 
-    [if not l1.next and l2: l1.next, l2.next = l2.next, l1.next] means that whenever 
-    we reach the tail of l1, but did not reach the end of l2 yet, we will swap the links
-    so that l1 becomes the longer list.
-        '''
-        carry, head = 0, l1
-        while l1 or l2:
-            if not l1.next and l2: l1.next, l2.next = l2.next, l1.next
-            
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            
-            carry, l1.val = divmod(val1 + val2 + carry, 10)
-            
-            prev = l1
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-                
-        if carry: prev.next = ListNode(carry)
-        return head
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
