da56d35dca0b02c28508746bb52ac2a1cf28f297
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2022-11-04 21:54:52 -0400

    Time: 69 ms (19.23%), Space: 13.8 MB (98.11%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,30 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        n = len(nums)
-        num1 = nums[1:]
-        num2 = nums[0:n - 1]
-        
-        memo1 = {}
-        memo2 = {}
-        
-        def dfs(i, nums, memo):
-            if i in memo:
-                return memo[i]
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
             
-            if not (0 <= i < len(nums)):
-                return 0
-            
-            # On peut décider d'inclure le num présent ou de passer au prochain
-            incl = nums[i] + dfs(i + 2, nums, memo)
-            excl = 0 + dfs(i + 1, nums, memo)
-            
-            memo[i] = max(incl, excl)
-            
-            return memo[i]
-        
-        if n != 1:
-            return max(dfs(0, num1, memo1), dfs(0, num2, memo2))
-        else:
-            return dfs(0, nums, memo1)
-            
+        return rob2
