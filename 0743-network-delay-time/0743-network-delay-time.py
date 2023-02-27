28b2b5599895ce01502a2d25b2714f68a432f007
Rayane Sahi <76753387+LKrs13@users.noreply.github.com>
2023-02-27 00:49:39 -0500

    Time: 5379 ms (5.04%), Space: 19.5 MB (10.87%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,14 +1,25 @@
+from collections import defaultdict, deque
+
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        signals = defaultdict(lambda: float('inf'))
+        signals[k] = 0
+        visited = set()
         graph = defaultdict(list)
         for u, v, w in times:
             graph[u].append((v, w))
-        queue = deque([(0, k)])
-        visited = {}
+        
+        queue = deque([(k, 0)])
         while queue:
-            t, u = queue.popleft()
-            if u not in visited or t < visited[u]:
-                visited[u] = t
-                for v, w in graph[u]:
-                    queue.append((t + w, v))
-        return -1 if len(visited) < n else max(visited.values())
+            node, cost = queue.popleft()
+            for nextNode, nextCost in graph[node]:
+                signals[nextNode] = min(signals[nextNode], cost + nextCost)
+                if (node, nextNode, signals[nextNode]) in visited:
+                    continue
+                visited.add((node, nextNode, signals[nextNode]))
+                queue.append((nextNode, signals[nextNode]))
+        
+        if len(signals) != n:
+            return -1
+        
+        return max(signals.values())
